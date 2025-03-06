a1d7689e21d036f7ae278c1e2c789711cc13f46a
monokkai <egormayer08@mail.ru>
2025-03-06 23:12:33 +0200

    added start of list of users between button and list

--- frontend/src/components/FieldInfo.js
+++ frontend/src/components/FieldInfo.js
@@ -4,9 +4,18 @@ import "./FieldInfo.css";
 const FieldInfo = () => {
   return (
     <div className="input-container">
-        <h1>Mayer Check</h1>
-        <p style={{fontSize: 16}}>Get full info about any person you want</p>
-      <input type="text" className="input-field" placeholder="Enter the ID of the person you've looking for..."/>
+      <h1>Mayer Check</h1>
+      <p style={{ fontSize: 16 }}>Get full info about any person you want</p>
+      <input
+        type="text"
+        className="input-field"
+        placeholder="Enter the ID of the person you've looking for..."
+      />
+
+      {
+        //список выведенных user'ов
+      }
+
       <button className="input-button">Get person</button>
     </div>
   );
